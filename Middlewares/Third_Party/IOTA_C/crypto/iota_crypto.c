cafc5a79d27b9ff0aa6e43a88895443a288452f6
Andrea PALMIERI <andrea.palmieri@st.com>
2021-03-17 10:12:57 +0100

    Get ready for alternative crypto lib
    Signed-off-by: Andrea PALMIERI <andrea.palmieri@st.com>

--- Middlewares/Third_Party/IOTA_C/crypto/iota_crypto.c
+++ Middlewares/Third_Party/IOTA_C/crypto/iota_crypto.c
@@ -1,30 +1,60 @@
 // Copyright 2020 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+#ifdef CRYPTO_USE_SODIUM
 #include "sodium.h"
+#else
+#include <stdint.h>
+#include <string.h>
+//#include "blake2.h"
+#endif
 
 #include "iota_crypto.h"
 
-void iota_crypto_randombytes(uint8_t *const buf, const size_t len) { randombytes_buf((void *)buf, len); }
+extern struct __RNG_HandleTypeDef hrng;
+int iota_rng_raw(void *data, uint8_t *output, size_t len);
+
+void iota_crypto_randombytes(uint8_t *const buf, const size_t len) {
+  iota_rng_raw(&hrng, buf, len);
+}
 
 // get ed25519 public and private key from address
 void iota_crypto_keypair(uint8_t const seed[], iota_keypair_t *keypair) {
+#if CRYPTO_USE_SODIUM
   crypto_sign_seed_keypair(keypair->pub, keypair->priv, seed);
+#else
+#endif
 }
 
 int iota_crypto_sign(uint8_t const priv_key[], uint8_t msg[], size_t msg_len, uint8_t signature[]) {
+#if CRYPTO_USE_SODIUM
   unsigned long long sign_len = ED_SIGNATURE_BYTES;
   return crypto_sign_ed25519_detached(signature, &sign_len, msg, msg_len, priv_key);
+#else
+  return 0;
+#endif
 }
 
 int iota_crypto_hmacsha256(uint8_t const secret_key[], uint8_t msg[], size_t msg_len, uint8_t auth[]) {
+#if CRYPTO_USE_SODIUM
   return crypto_auth_hmacsha256(auth, msg, msg_len, secret_key);
+#else
+  return 0;
+#endif
 }
 
 int iota_crypto_hmacsha512(uint8_t const secret_key[], uint8_t msg[], size_t msg_len, uint8_t auth[]) {
+#if CRYPTO_USE_SODIUM
   return crypto_auth_hmacsha512(auth, msg, msg_len, secret_key);
+#else
+  return 0;
+#endif
 }
 
 int iota_blake2b_sum(uint8_t const msg[], size_t msg_len, uint8_t out[], size_t out_len) {
+#if CRYPTO_USE_SODIUM
   return crypto_generichash_blake2b(out, out_len, msg, msg_len, NULL, 0);
+#else
+  return blake2b(out, out_len, msg, msg_len, NULL, 0);
+#endif
 }
--- Middlewares/Third_Party/IOTA_C/crypto/iota_crypto.h
+++ Middlewares/Third_Party/IOTA_C/crypto/iota_crypto.h
@@ -4,8 +4,6 @@
 #ifndef __CRYPTO_IOTA_H__
 #define __CRYPTO_IOTA_H__
 
-#include "sodium.h"
-
 #define ED_SEED_BYTES 32         // ed25519 seed bytes
 #define ED_PUBLIC_KEY_BYTES 32   // ed2519 public key bytes
 #define ED_PRIVATE_KEY_BYTES 64  // ed25519 secret/private key bytes
